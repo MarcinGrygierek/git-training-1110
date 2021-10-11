9de876aa22f0fb9e30306c7e2c086aab7c2c7175
Marcin Grygierek <grygierek.marcin@gmail.com>
2021-10-11 13:34:57 +0200

    Ready authorization functionality

---
+++ auth.js
@@ -0,0 +1,8 @@
+console.log('There will be authorization');
+
+const login = 'admin';
+const pwd = 'password';
+
+const logIn = () => {
+    console.log('Logging in');
+}
--- test.js
+++ test.js
@@ -1,11 +1,12 @@
 console.log('Hello world');
 console.log('Test');
-console.log('asadsd')
+console.log('asadsd');
 console.log('Hello world2');
 console.log('TEEEEEST');
 console.log('Hello world3');
 console.log('Test22222');
+console.log('test');
 console.log('Hello world3');
 console.log('Test3');
-const a = 2
+const a = 2;
 console.log('test');
