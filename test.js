2fadff72c9380300aa47fb7e0c2988ce0bf14dfb
Marcin Grygierek <grygierek.marcin@gmail.com>
2021-10-11 13:31:26 +0200

    new changes

--- test.js
+++ test.js
@@ -1,9 +1,11 @@
 console.log('Hello world');
 console.log('Test');
+console.log('asadsd')
 console.log('Hello world2');
 console.log('TEEEEEST');
 console.log('Hello world3');
 console.log('Test22222');
 console.log('Hello world3');
 console.log('Test3');
+const a = 2
 console.log('test');
